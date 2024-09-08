8e8a896f72fbb19aceb3f7f19b4df299a93c833c
Thouma <richard_allcca_llano@hotmail.com>
2024-09-07 22:40:06 -0500

    added test for envs.plugins.test init and refactor to mongo-log.datasource.impl

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -6,21 +6,35 @@ interface IConnectionOptions {
 }
 
 export class MongoDatabase {
+  private static isConnected: boolean = false;
 
   static async connect(options: IConnectionOptions) {
     const { mongoUrl, dbName } = options;
 
-    try {
+    if (this.isConnected) {
+      console.log('Already connected to MongoDB');
+      return true; // Ya está conectado
+    }
 
+    try {
       await mongoose.connect(mongoUrl, {
-        dbName
+        dbName,
       });
 
+      this.isConnected = true; // Marca como conectado
       console.log('Connected to MongoDB');
-
+      return true;
     } catch (error) {
       console.log(error);
       throw new Error('Error connecting to MongoDB');
+    }
+  }
+
+  static async disconnect() {
+    if (this.isConnected) {
+      await mongoose.connection.close();
+      this.isConnected = false; // Marca como desconectado
+      console.log('Disconnected from MongoDB');
     }
   }
 }
